0bdcf5b47f4735498afce2fb083b9d707d531795
Eric Cobb <eric.cobb@elastic.co>
2021-11-02 19:10:29 -0500

    Trying again

--- ADSBCommand.py
+++ ADSBCommand.py
@@ -1,10 +1,11 @@
+import argparse
 import ADSBSensor
 import csv
 import random
 from random import randint
 from time import sleep
 
-adsb_sensors = []
+adsb_sensors = {}
 
 def setup():
     global adsb_sensors
@@ -15,17 +16,34 @@ def setup():
 
         # For each ADS-B row in CSV file, create ADSBSensor object
         for row in reader:
-            adsb_sensors.append(ADSBSensor.ADSBSensor(row['id'], row['lat'], row['lon'], row['city'], row['state'], row['tier']))
+            obj = ADSBSensor.ADSBSensor(row['id'], row['lat'], row['lon'], row['city'], row['state'], row['tier'])
+            adsb_sensors[obj.id] = obj
 
 def poll_sensors():
     global adsb_sensors
 
+    # Randomize access to the objects in this dictionary
     adsb_copy = adsb_sensors.copy()
-    random.shuffle(adsb_copy)
-    while len(adsb_copy) != 0:
-        print(adsb_copy.pop().status())
+    keys = list(adsb_copy.keys())
+    random.shuffle(keys)
+    
+    for key in keys:
+        print(adsb_copy[key].status())
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog='ADSBCommand.py', description='Parse command-line arguments')
+    parser.add_argument('--add-latency', dest='latency', action='store', help='Add latency to the return of ADSBSensor object\'s status')
+    parser.add_argument('--sensor-offline', dest='offline', action='store_false', help='Make the sensor start in Offline mode (True or False)')
+    parser.add_argument('--flux-capacitor', dest='flux_cap', action='store', type=float, help='Set the initial flux capacitor value')
+    parser.parse_args()
+
+def main():
+    global adsb_sensors
+    actions = ['set_health', 'set_flux_cap', 'set_offline']
+    poll_sensors()
 
 if __name__ == '__main__':
+    parse_args()
     setup()
-    poll_sensors()
+    main()
     
--- ADSBSensor.py
+++ ADSBSensor.py
@@ -7,8 +7,9 @@ offline = False
 healthy = True
 flux_cap = 1.21
 status = 'Operating nominally'
+latency = 0
 outages = [
-    '{"outage.category": "power", "outage.message": "Great Scott! Flux capacitor surge detected (1.22 GigaWatt). Shutting down to avoid going back to 2051."}',
+    '{"outage.category": "power", "outage.message": "Great Scott! Flux capacitor surge detected (' + str(flux_cap) + 'GigaWatt). Shutting down to avoid going back to 2051."}',
     '{"outage.category": "endurance", "outage.message": "This is hard, and I\'m tired. Going offline."}',
     '{"outage.category": "power", "outage.message": "My battery is low and it\'s getting dark."}',
     '{"outage.category": "software", "outage.message": "Task failed successfully."}',
@@ -35,27 +36,38 @@ class ADSBSensor:
         healthy = health
 
     def set_offline(self, state):
-        global offline
+        global offline, flux_cap, status
         offline = state
+        flux_cap = 0.0
+        if offline:
+            status = 'Offline'
+    
+    def set_flux_cap(self, flux_cap_value):
+        global flux_cap, status
+        flux_cap = flux_cap_value
+        if flux_cap > 1.21:
+            set_healthy(False)
+            status = outages[0]
+
+    def set_latency(self, new_latency):
+        global latency
+        latency = new_latency
 
     def status(self):
         global status
         global outages
         global flux_cap
+        global latency
         timestamp = datetime.now()
-        if offline:
-            status = 'Offline'
-            flux_cap = 0.0
-        if not healthy:
-            status = random.choice(outages)
         msg = '[{"timestamp": "%s", "id": "%s", "city": "%s", "state": "%s", "location": {"latitude": "%s", "longitude": "%s"}, "tier": "%s", "flux_capacitor": "%s", "status": "%s"}]' % (
             timestamp,
             self.id, self.city,
             self.state, self.latitude, 
             self.longitude, self.tier,
             flux_cap, status)
+        
         # Artificially add latency
-        sleep(randint(1,30))
+        sleep(latency)
         return msg 
 
     def __str__():
