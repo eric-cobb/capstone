f215cd332e2ceb88b14761fe2fa12a1f6b48cc9d
Eric Cobb <eric.cobb@elastic.co>
2021-12-29 11:47:27 -0600

    Changed outage categories to be uppercase

--- outages.py
+++ outages.py
@@ -1,36 +1,36 @@
 flux_msgs = [
     {
-        "outage.category": "power", 
+        "outage.category": "Power", 
         "outage.message": "Great Scott! Flux capacitor surge detected ({} GigaWatt). Shutting down to avoid going back to 2051." 
     }
 ]
 outages = [ 
     {
-        "outage.category": "endurance",
+        "outage.category": "Endurance",
         "outage.message": "This is hard, and I\'m tired. Going offline."
     }, 
     {
-        "outage.category": "power",
+        "outage.category": "Eower",
         "outage.message": "My battery is low and it\'s getting dark."
     }, 
     {
-        "outage.category": "software", 
+        "outage.category": "Software", 
         "outage.message": "Task failed successfully." 
     }, 
     {
-        "outage.category": "software",
+        "outage.category": "Software",
         "outage.message": "There\'s a glitch in the matrix."
     },
     {
-        "outage.category": "power",
+        "outage.category": "Power",
         "outage.message": "Vaccuum tube failure."
     },
     {
-        "outage.category": "power",
+        "outage.category": "Power",
         "outage.message": "Retro Encabulator has died of dysentery"
     },
     {
-        "outage.category": "power",
+        "outage.category": "Power",
         "outage.message": "Unilateral Phase Detractor unable to synchronize with Cardinal Gram Meters"
     }
 ]
