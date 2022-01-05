a29d0585c6ececbfff262306924b939eb1d9f24f
Eric Cobb <eric.cobb@elastic.co>
2022-01-05 10:51:03 -0600

    Updated the python script to be executed from bulk_load.py to bulk_load_accidents.py

--- bin/bulk_load_wrapper.sh
+++ bin/bulk_load_wrapper.sh
@@ -2,5 +2,5 @@
 
 for year in $(seq 1990 2021); do
     echo "Bulk loading year $year"
-  python3 bulk_load.py output/cases$year.ndjson
+  python3 bulk_load_accidents.py output/cases$year.ndjson
 done
