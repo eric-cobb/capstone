6f57b455b41d75d29e1efa988b2481230eef932f
Eric Cobb <eric.cobb@elastic.co>
2022-01-05 11:50:04 -0600

    Updated the file paths to be more portal by using a variable for the basepath

--- bin/bulk_load_adsb_wrapper.sh
+++ bin/bulk_load_adsb_wrapper.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#
+# This is just a wrapper script used as a cron job to run the 
+# ADSBCommand.py script at regular intervals with various options for
+# latency, error rates, and flux capacitor issues
+#######################################################################
 
-#flux_values=("1.31" "1.58" "1.22" "1.20" "1.16" "1.33" "1.55" "1.56" "1.32" "1.18" "1.19" "1.23")
+basedir="/home/ericcobb/capstone"
 flux_values=("1" "2" "3" "4" "5" "6" "15" "17" "28" "19" "22" "45" "47" "61" "92" "14" "26" "49" "72" "77")
 size=${#flux_values[@]}
 index=$(($RANDOM % $size))
@@ -11,16 +16,15 @@ sindex=$(($RANDOM % $ssize))
 
 if (( RANDOM % 2 == 0 )); then
   if (( RANDOM % 2 == 0 )); then
-    /usr/bin/python3 /home/ericcobb/capstone/ADSBCommand.py --add-latency --file /home/ericcobb/capstone/data/adsb/adsb.ndjson
+    /usr/bin/python3 $basedir/ADSBCommand.py --add-latency --file $basedir/data/adsb/adsb.ndjson
   else
-    #/usr/bin/python3 /home/ericcobb/capstone/ADSBCommand.py --add-latency --sensor ${sensor_values[$sindex]} --flux-capacitor ${flux_values[$index]} --file /home/ericcobb/capstone/data/adsb/adsb.ndjson
-    /usr/bin/python3 /home/ericcobb/capstone/ADSBCommand.py --add-latency --random-flux ${flux_values[$index]} --file /home/ericcobb/capstone/data/adsb/adsb.ndjson
+    /usr/bin/python3 $basedir/ADSBCommand.py --add-latency --random-flux ${flux_values[$index]} --file $basedir/data/adsb/adsb.ndjson
   fi
 else
-  /usr/bin/python3 /home/ericcobb/capstone/ADSBCommand.py --add-latency --error-rate $(( ( RANDOM % 6 )  + 1 )) --file /home/ericcobb/capstone/data/adsb/adsb.ndjson
+  /usr/bin/python3 $basedir/ADSBCommand.py --add-latency --error-rate $(( ( RANDOM % 6 )  + 1 )) --file $basedir/data/adsb/adsb.ndjson
 fi
 if [ $? -eq 0 ]; then
-  /usr/bin/python3 /home/ericcobb/capstone/bin/bulk_load_adsb.py /home/ericcobb/capstone/data/adsb/adsb.ndjson
+  /usr/bin/python3 $basedir/bin/bulk_load_adsb.py $basedir/data/adsb/adsb.ndjson
   if [ $? -eq 0 ]; then
     echo "OK"
   else
